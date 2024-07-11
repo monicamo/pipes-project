93c3e400c96efcb9451afdde2dbf8bbe0f8a1388
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-11 12:36:43 -0300

    Chaining Pipes & Being Aware of Limitations

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,8 +6,41 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return value + '-transform';
-  }
+  transform(value: string | number | null, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah'): unknown {
+    let val: number;
 
+    if (!value)
+      return value
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+
+    if (inputType === 'cel' && outputType === 'fah')
+    {
+      outputTemp = val * (9/5) + 32;
+    }
+    else if (inputType === 'fah' && outputType === 'cel')
+    {
+      outputTemp =(val - 32) * (5/9);
+    }
+    else
+    {
+      outputTemp = val;
+    }
+
+    let symbol: '℃' | '℉';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '℃' : '℉';
+    } else {
+      symbol = outputType === 'cel' ? '℃' : '℉';
+    }
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
+  }
 }
